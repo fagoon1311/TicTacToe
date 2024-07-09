445b22ddfec6231fdea988cfa8f9ab98295a0b28
fagoon1311 <fs14smailbox65@gmail.com>
2024-07-09 19:27:51 +0530

    checkwinner dynamic

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import Tictac from './components/TicTac.js';
 
 function App() {
   return (
-    <div className="App flex flex-col items-center absolute justify-start -z-10 h-full w-full bg-gradient-to-b from-zinc-900  to-cyan-900">
+    <div className="App flex flex-col items-center justify-start h-full w-full bg-gradient-to-b from-zinc-900 to-cyan-900">
       <h1 className="text-6xl font-bold text-cyan-400 mt-4">TIC-TAC-TOE</h1>
       <div className="flex items-center justify-center flex-row mt-10">
         <Tictac />
       </div>
     </div>
   );
-  
 }
+
+
 
 export default App;
 
--- src/components/TicTac.js
+++ src/components/TicTac.js
@@ -2,14 +2,16 @@ import React, { useState } from 'react'
 import {useWinningPatterns} from '../utils/useWinningPatterns'
 
 const Tictac = () => {
-    const size = 3
+    // grid size default 3
+    const [size, setSize] = useState(4)
     const initialboard = Array(size*size).fill(null)
-  
+
     const [board, setBoard] = useState(initialboard)
     const [turnX, setTurnX] = useState(true) // first turn will be of X always
 
     // winning patterns for 3*3 board game.
     const winningPatterns = useWinningPatterns(size)
+    //console.log(winningPatterns)
 
     // resets the game board back to initial state.
     const resetGame = () => {
@@ -18,15 +20,26 @@ const Tictac = () => {
     }
 
     // to check if someone has actually won the game.
+    // const checkWinner = (board) => {
+    //     for(let i = 0; i < winningPatterns.length; i++){
+    //         const [a, b, c] = winningPatterns[i]
+    //         if(board[a] && board[a]===board[b] && board[a]===board[c]){
+    //             return board[a]
+    //         }
+    //     }
+    //     return null
+    // }
+
     const checkWinner = (board) => {
         for(let i = 0; i < winningPatterns.length; i++){
-            const [a, b, c] = winningPatterns[i]
-            if(board[a] && board[a]===board[b] && board[a]===board[c]){
-                return board[a]
+            let first = board[winningPatterns[i][0]];
+            let isWinningLine = first && winningPatterns[i].every(index => board[index] === first);
+            if (isWinningLine) {
+                return first
             }
         }
         return null
-    }
+        }
 
     const handleClick = (index) =>{
         const winner = checkWinner(board)
@@ -42,6 +55,14 @@ const Tictac = () => {
         if(!board.includes(null)) return `It's a draw`
         else return `Player ${turnX ? "X's": "O's"} turn`
     }
+    const gridClasses = {
+        1: 'grid grid-flow-row grid-cols-1 grid-rows-1 gap-4 mb-10',
+        2: 'grid grid-flow-row grid-cols-2 grid-rows-2 gap-4 mb-10',
+        3: 'grid grid-flow-row grid-cols-3 grid-rows-3 gap-4 mb-10',
+        4: 'grid grid-flow-row grid-cols-4 grid-rows-4 gap-4 mb-10',
+        5: 'grid grid-flow-row grid-cols-5 grid-rows-5 gap-4 mb-10',
+        6: 'grid grid-flow-row grid-cols-6 grid-rows-6 gap-4 mb-10',
+      };
 
 
 
@@ -50,10 +71,10 @@ const Tictac = () => {
         
         <span className='text-white text-xl mb-10 bg-cyan-900 rounded-md p-2'>{statusMessage()}</span>
         
-        <div className='grid grid-flow-row grid-cols-3 grid-rows-3 gap-4 mb-10'>
+        <div className={gridClasses[size]}>
             {
                 board.map((val,index)=><button 
-                className='bg-sky-700 bg-opacity-20 rounded-md h-32 w-32 text-2xl text-white  hover:border border-cyan-400'
+                className='bg-sky-700 bg-opacity-20 rounded-md h-24 w-24 text-2xl text-white  hover:border border-cyan-400'
                 key={index}
                 onClick={()=>handleClick(index)}
                 disabled={val!==null}
